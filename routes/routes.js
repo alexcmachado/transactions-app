161178a5a0b578d233e21a90a13b27e7d6bb1c2e
Alex Costa Machado <alexcmachado@gmail.com>
2021-05-02 17:47:32 -0300

    Create GET route

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,18 @@
-import retrieveTransaction from "../services/transactionService.js";
+import {
+  createTransaction,
+  retrieveTransaction,
+  updateTransaction,
+  deleteTransaction,
+} from "../services/transactionService.js";
 import { Router } from "express";
 const transactionRouter = Router();
 
+transactionRouter.post("/", createTransaction);
+
 transactionRouter.get("/", retrieveTransaction);
+
+transactionRouter.put("/", updateTransaction);
+
+transactionRouter.delete("/", deleteTransaction);
 
 export default transactionRouter;
--- services/transactionService.js
+++ services/transactionService.js
@@ -3,8 +3,50 @@ const ObjectId = mongoose.Types.ObjectId;
 
 import transactionModel from "../models/transactionModel.js";
 
-const retrieveTransaction = async (req, res) => {
-  res.send("Hello, World!");
+const createTransaction = async (req, res) => {
+  try {
+    res.send("Hello, World!");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-export default retrieveTransaction;
+const retrieveTransaction = async (req, res) => {
+  try {
+    const { period } = req.query;
+
+    if (!period) {
+      throw new Error(
+        "Please inform a period for your request. Format: yyyy-mm"
+      );
+    }
+
+    const transactions = await transactionModel.find({ yearMonth: period });
+    res.send(transactions);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const updateTransaction = async (req, res) => {
+  try {
+    res.send("Hello, World!");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const deleteTransaction = async (req, res) => {
+  try {
+    res.send("Hello, World!");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export {
+  createTransaction,
+  retrieveTransaction,
+  updateTransaction,
+  deleteTransaction,
+};
